3cc0f07a87e0ea8f56ebc5651ebf06532d376be0
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-04-04 09:11:10 -0300

    form

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,22 @@ const { createApp } = Vue
 
 const app = createApp({
     data() {
+        return{
+            formulario:''
+        }
 
     },
     created() {
+        this.probando()
 
     },
     mounted() {
 
     },
     methods: {
+       probando(){
+        
+       }
 
     },
     computed: {
